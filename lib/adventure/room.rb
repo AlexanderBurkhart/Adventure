207688171da83b026a3230d7338340157936f108
Alexander Burkhart <alexander.burkhart@gmail.com>
2010-08-15 15:29:11 -0400

    added inventory to Room object

--- lib/adventure/room.rb
+++ lib/adventure/room.rb
@@ -1,9 +1,18 @@
 class Room
-  attr_accessor :title, :description
+  attr_accessor :title, :description, :inventory
   
-  def initialize(title, description)
+  def initialize(title, description, inventory)
     self.title = title
     self.description = description
+    self.inventory = inventory
+  end
+  
+  def items_description
+    if inventory.size > 1
+      "There is " + inventory[0 .. -2].join(", ") + ", and " + inventory[-1] + " in the room."
+    else
+      "There is " + inventory[0] + " in the room."
+    end
   end
   
 end
--- test/test_room.rb
+++ test/test_room.rb
@@ -5,7 +5,8 @@ require "adventure/room"
 class TestAdventureRoom < Test::Unit::TestCase
   def test_create_room
     room = Room.new("Living Room",
-                    "You are in the living room now.  You see two couches, one tv, and one wii.")
+                    "You are in the living room now.  You see two couches, one tv, and one wii.",
+                    ['a glowing sword'])
     
     assert_instance_of Room, room
     
@@ -15,5 +16,22 @@ class TestAdventureRoom < Test::Unit::TestCase
     # Room must have a description
     assert_equal "You are in the living room now.  You see two couches, one tv, and one wii.", room.description 
     
+    # Room must have an inventory
+    assert_equal ['a glowing sword'], room.inventory
+    
+    # Room must have an items description
+    assert_equal "There is a glowing sword in the room.", room.items_description
+    
+    # Add item to room inventory
+    room.inventory << "a gold coin"
+    
+    # Room must have an items description
+    assert_equal "There is a glowing sword, and a gold coin in the room.", room.items_description
+    
+    # Add item to room inventory
+    room.inventory << "a lego piece"
+    
+    # Room must have an items description
+    assert_equal "There is a glowing sword, a gold coin, and a lego piece in the room.", room.items_description
    end
 end
